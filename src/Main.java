135d94569a73e4c52cca71d05504df952df728fe
Enes <enesakbulut1933@gmail.com>
2022-07-28 14:14:12 +0300

    CalculatingExponentsWithRecursiveMethods

---
+++ src/Main.java
@@ -0,0 +1,34 @@
+import java.util.Scanner;
+
+public class Main {
+
+        static int result = 1;
+
+        static int exp(int sayi, int us) {
+
+            if (us == 0) {
+                return 1;
+            }
+            result *= sayi;
+            exp(sayi, us - 1);
+            return result;
+        }
+
+
+        public static void main(String[] args) {
+
+
+            Scanner input = new Scanner(System.in);
+            System.out.println(" us bir sayi giriniz");
+            int exp = input.nextInt();
+            System.out.println(" sayi  sayisini giriniz :");
+            int num = input.nextInt();
+
+            System.out.println(exp(num, exp));
+
+            
+
+
+
+    }
+}
